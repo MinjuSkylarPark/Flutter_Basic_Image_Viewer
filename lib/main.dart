8059107f53577f9eff8e066d61e1874aedf81ffc
SkylarPark <80894789+MinjuSkylarPark@users.noreply.github.com>
2023-10-07 23:23:27 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Kitty party'),
+        ),
+        body: Center(
+          child: Image.asset('assets/cat7.jpg')
+          // Replace 'sample.jpg' with your image file name
+        
+        ),
+      ),
+    );
+  }
+}
