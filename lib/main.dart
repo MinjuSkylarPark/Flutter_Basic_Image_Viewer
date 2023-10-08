c96745e90a61265ab9451c4f2949f20ab47643d5
SkylarPark <80894789+MinjuSkylarPark@users.noreply.github.com>
2023-10-08 21:17:01 +0900

    added vertical scroll function

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Kitty party'),
+          title: Text('Image Viewer'),
         ),
-        body: Center(
-          child: Image.asset('assets/cat7.jpg')
-          // Replace 'sample.jpg' with your image file name
-        
-        ),
+        body: ImageListView(), // Use a custom widget for displaying images
       ),
+    );
+  }
+}
+
+class ImageListView extends StatelessWidget {
+  final List<String> imagePaths = [
+    'assets/cat.jpg', // Replace with your image paths
+    'assets/cat7.jpg',
+    'assets/funnycat.jpeg',
+    // Add more image paths as needed
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical, // Scroll horizontally
+      itemCount: imagePaths.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0), // Add spacing between images
+          child: Image.asset(imagePaths[index]),
+        );
+      },
     );
   }
 }
